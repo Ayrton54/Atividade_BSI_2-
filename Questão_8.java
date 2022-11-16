30d6812e8be6e4b8fafcb783a01a037178a7b4e4
Ayrton Lopes <106751266+Ayrton54@users.noreply.github.com>
2022-11-16 20:41:53 -0300

    Update Questão_8.java

--- Questão_8.java
+++ Questão_8.java
@@ -6,6 +6,21 @@ public class Questão_8 {
 
    
     public static void main(String args[]) {
-        // TODO code application logic here
+        double n1, n2, n3, n4 = 0, n5;
+        
+        n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite um valor"));
+        
+        for(int i = 2; i < n1; i++)
+        {
+            i = i * (i - 1);
+            
+            n2 = i;
+            
+            n3 = 1 / n2;
+            
+            n4 = n4 + n3;
+        }
+        n5 = n4 + 2;
+        System.out.println( "O valor da costante é = " + n5);
     }
 }
